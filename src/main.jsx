bf5c4fcf15a47b6be8a02c8d62e8f66af4ae4629
SrRomance2025 <rioslugojo@gmail.com>
2025-04-24 12:05:13 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SrRomanceMicrositio from './SrRomanceMicrositio.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SrRomanceMicrositio />
+  </React.StrictMode>
+)
